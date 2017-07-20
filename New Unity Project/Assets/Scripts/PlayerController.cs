2b95b0cf10c8599e4efac5a333b4ae6ee97baf4b
Luca Falco <thefixer999@gmail.com>
2017-07-20 12:34:34 +0200

    Commit iniziale

---
+++ New Unity Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    //2 funzioni movimento per le 2 modalità
+    //Sparo
+    //Melee in top down
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
