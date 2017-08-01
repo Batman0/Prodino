713cdf52553cd377750e26b5edb6d40461729dd8
CARLO\user2 <carlo93.cg@gmail.com>
2017-08-01 23:15:11 +0200

    Prova Cambio Pose

--- Assets/Scripts/Morph.cs
+++ Assets/Scripts/Morph.cs
@@ -2,15 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Morph : MonoBehaviour {
+public class Morph : MonoBehaviour
+{
+    private Animator ani;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Awake ()
+    {
+        ani = GetComponent<Animator>();
 	}
+    void Start()
+    {
+        ani.SetBool("IsFlying", false);
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        if(Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.forward * -10 * Time.deltaTime);
+        }
+
+		if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(ani.GetBool("IsFlying")==false)
+            {
+                ani.SetBool("IsFlying", true);
+            }
+            else
+            {
+                ani.SetBool("IsFlying", false);
+            }
+        }
 	}
 }
