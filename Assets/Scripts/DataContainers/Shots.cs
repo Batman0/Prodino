3a91fd4a87d72abb46b408167139202c3098ee5b
CARLO\user2 <carlo93.cg@gmail.com>
2017-10-07 18:06:47 +0200

    SphericalAiming shot (da finire)

--- Assets/Scripts/DataContainers/Shots.cs
+++ Assets/Scripts/DataContainers/Shots.cs
@@ -21,12 +21,17 @@ public static class Shots
         //bullet.layer = layer;
     }
 
-    public static GameObject ShootLaser(GameObject prefab, Transform bulletSpawnpoint, Transform rotTransform, float width, float height)
+    public static void ShootLaser(GameObject prefab, Transform bulletSpawnpoint, Transform rotTransform, float width, float height)
     {
-        GameObject bullet = Object.Instantiate(prefab, bulletSpawnpoint.position, rotTransform.rotation) as GameObject;
-        bullet.transform.localScale = new Vector3(width, height, bullet.transform.localScale.z);
-        return bullet;
+        GameObject laser = Object.Instantiate(prefab, bulletSpawnpoint.position, rotTransform.rotation) as GameObject;
+        laser.transform.localScale = new Vector3(width, height, laser.transform.localScale.z);
+        laser.transform.SetParent(bulletSpawnpoint.parent);
     }
+
+    /*public static void ShootAiming(GameObject prefab,Transform bulletSpawnpoint, Transform rotTransform)
+    {
+       
+    }*/
 
     public static void ShootTrail()
     {
@@ -38,7 +43,7 @@ public static class Shots
         GameObject bomb = Object.Instantiate(prefab, spawnpoint.position, rotTransform.rotation);
     }
 
-    public static void Shoot(ShotType shotType, Properties properties, ref float timer, ref bool canShoot, Transform spawnPoint, Transform rotTransform)
+    public static void Shoot(ShotType shotType, Properties properties, ref float timer, ref bool canShoot, ref bool rotateRight, Transform spawnPoint, Transform rotTransform, Transform transform)
     {
         switch (shotType)
         {
@@ -74,8 +79,7 @@ public static class Shots
                         }
                         if (canShoot)
                         {
-                            GameObject laser = ShootLaser(properties.enemyLaserPrefab, spawnPoint, rotTransform, properties.l_LaserDepth, properties.l_LaserHeight);
-                            laser.transform.SetParent(spawnPoint.parent);
+                            ShootLaser(properties.enemyLaserPrefab, spawnPoint, rotTransform, properties.l_LaserDepth, properties.l_LaserHeight);
                             canShoot = false;
                         }
                         if (timer < properties.l_WaitingTime + properties.l_LoadingTime + properties.l_ShootingTime)
@@ -91,6 +95,40 @@ public static class Shots
                 }
                 break;
             case ShotType.SPHERICALAIMING:
+                if(GameManager.instance.currentGameMode == GameMode.SIDESCROLL)
+                {    
+                    rotTransform.LookAt(Register.instance.player.transform);
+                    
+                    
+                    if (timer < properties.sa_FireRate)
+                    {
+                        timer += Time.deltaTime;
+                    }
+                    else
+                    { 
+                        ShootForward(properties.enemyBulletPrefab, spawnPoint, rotTransform);
+                        timer = 0.0f;
+                    }
+                }
+                else
+                {
+                    if(Register.instance.player.transform.position.x >= transform.position.x)
+                    {
+                        if(rotateRight)
+                        {
+                            rotTransform.RotateAround(transform.position, Vector3.forward, 180);
+                            rotateRight = false;
+                        }
+                    }
+                    else
+                    {
+                        if(!rotateRight)
+                        {
+                            rotTransform.RotateAround(transform.position, Vector3.forward, 180);
+                            rotateRight = true;
+                        }
+                    }
+                }             
                 break;
             case ShotType.BOMBDROP:
                 if (timer < properties.bd_LoadingTime)
