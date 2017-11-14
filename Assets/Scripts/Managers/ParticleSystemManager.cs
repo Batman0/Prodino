5b382e372fd13c47db7cdf1fceb530dcc2808bec
gullpacco <gullpacco@gmail.com>
2017-11-14 11:06:30 +0100

    particle system manager

---
+++ Assets/Scripts/Managers/ParticleSystemManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSystemManager : MonoBehaviour
+{
+
+    ParticleSystem[] particles;
+
+    void Awake()
+    {
+        particles = GetComponentsInChildren<ParticleSystem>();
+    }
+
+    public void PlayAll()
+    {
+        foreach (ParticleSystem particle in particles)
+        {
+            particle.Play();
+        }
+    }
+    public void StopAll()
+    {
+        foreach (ParticleSystem particle in particles)
+        {
+            particle.Stop();
+        }
+    }
+
+
+}
