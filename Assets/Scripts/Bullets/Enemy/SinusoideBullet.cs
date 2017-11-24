59f8ae9277cfe71cb06fa135a0f230274d8b93af
BatManBatManBatMan <imbatmanyear1@gmail.com>
2017-11-24 18:33:27 +0100

    Added Rewired to testlivellopietro_777 and modified sinusoide bullet movement

--- Assets/Scripts/Bullets/Enemy/SinusoideBullet.cs
+++ Assets/Scripts/Bullets/Enemy/SinusoideBullet.cs
@@ -8,9 +8,11 @@ public class SinusoideBullet : NormalBullet
     private bool moveForward;
     private float xSpeed;
     private float zSpeed;
+    private float zSpeedToIncrease;
     private float forwardDistance;
     private float backDistance;
     private float transformTargetDeltaDistance;
+    private float zOriginal;
     private Vector3 originalPos;
     private Vector3 target;
     private Transform playerTr;
@@ -41,6 +43,8 @@ public class SinusoideBullet : NormalBullet
         }
         target = moveForward ? new Vector3(transform.position.x, transform.position.y, originalPos.z + forwardDistance) : new Vector3(transform.position.x, transform.position.y, originalPos.z - backDistance);
         xSpeed = transform.position.x >= playerTr.position.x ? -speed : speed;
+        zOriginal = transform.position.z;
+        zSpeedToIncrease = 0;
     }
 
     protected override void Move()
@@ -49,7 +53,11 @@ public class SinusoideBullet : NormalBullet
         {
             moveForward = !moveForward;
             target = moveForward ? new Vector3(transform.position.x, transform.position.y, originalPos.z + forwardDistance) : new Vector3(transform.position.x, transform.position.y, originalPos.z - backDistance);
+            zOriginal = transform.position.z;
+            zSpeedToIncrease = 0;
         }
-        transform.position = new Vector3(transform.position.x + xSpeed * Time.fixedDeltaTime, transform.position.y, Mathfx.Hermite(transform.position.z, target.z, zSpeed * Time.fixedDeltaTime));
+        transform.position = new Vector3(transform.position.x + xSpeed * Time.fixedDeltaTime, transform.position.y, Mathfx.Hermite(zOriginal, target.z, zSpeedToIncrease));
+        //Debug.Log(Mathfx.Hermite(zOriginal, target.z, zSpeedToIncrease));
+        zSpeedToIncrease += Time.fixedDeltaTime / 3;
     }
 }
